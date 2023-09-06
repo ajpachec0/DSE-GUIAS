2a35c9efd571c218ff027eeab912965451e69ee8
Pacheco <pacheco@orbitweb.net>
2023-09-06 10:59:02 -0600

    Guia 1 y 2

---
+++ Guia 1/MvcPelicula/MvcPelicula/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MvcPelicula.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: /HelloWorld/Welcome
+
+        public ActionResult Welcome(string nombre, int numVeces = 1)
+        {
+            ViewBag.Mensaje = "Hola " + nombre;
+            ViewBag.numVeces = numVeces;
+
+            return View();
+        }
+
+    }
+}
